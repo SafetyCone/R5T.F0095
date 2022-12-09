acc4f9605f683bb82a8100dabae92585278e8394
davidcoats <david@getsafetycone.com>
2022-12-09 11:24:21 -0800

    Initial commit.

---
+++ source/R5T.F0095/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0095
+{
+	/// <summary>
+	/// Code file generation by copying files.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0095/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0095
+Code file generation by copying files.
